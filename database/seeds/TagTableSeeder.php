f4384fabfa3ac9a3d18951d81acb5a640ad45bd0
David Griffiths <dgriff_67@yahoo.com>
2016-12-16 16:11:52 +0000

    inserted array of tags and iterator to implement tag table seeder

--- database/seeds/TagTableSeeder.php
+++ database/seeds/TagTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Tag;
 
 class TagTableSeeder extends Seeder
 {
@@ -11,6 +12,27 @@ class TagTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $tags = array(
+            array('user_id' => '1','name' => 'glass'),
+            array('user_id' => '1','name' => 'beethoven'),
+            array('user_id' => '1','name' => 'bach'),
+            array('user_id' => '1','name' => 'soundtrack'),
+            array('user_id' => '1','name' => 'opera'),
+            array('user_id' => '1','name' => 'mozart'),
+            array('user_id' => '1','name' => 'requiem'),
+            array('user_id' => '1','name' => 'kasabian'),
+            array('user_id' => '1','name' => 'live'),
+            array('user_id' => '1','name' => 'bowie'),
+            array('user_id' => '1','name' => 'piano'),
+            array('user_id' => '1','name' => 'dvorak'),
+            array('user_id' => '1','name' => 'oasis')
+        );
+
+        foreach ($tags as $tag) {
+            Tag::create([
+                'name' => $tag['name'],
+                'user_id' => $tag['user_id']
+            ]);
+        }
     }
 }
