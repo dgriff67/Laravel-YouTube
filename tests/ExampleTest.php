bb077d9bc01ad47f66019ab17d722cb3c7a452a3
David Griffiths <dgriff_67@yahoo.com>
2016-12-16 17:24:15 +0000

    added test for favourites route assering that response return is OK

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -16,4 +16,10 @@ class ExampleTest extends TestCase
         $this->visit('/')
              ->see('Laravel');
     }
+
+    public function testFavouritesList()
+    {
+        $this->get(route('favourites'))
+             ->assertResponseOK();
+    }
 }
