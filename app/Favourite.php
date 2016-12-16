31f7ae76513a3ba7c37d5f091d4f7a8b3f19ef09
David Griffiths <dgriff_67@yahoo.com>
2016-12-16 12:05:22 +0000

    added fillable fields and relationship to User model, belongsTo

--- app/Favourite.php
+++ app/Favourite.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Favourite extends Model
 {
-    //
+    protected $fillable = [ 'title', 'videoid' ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+
 }
