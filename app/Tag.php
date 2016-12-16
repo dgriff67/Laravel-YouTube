e069e82f0b3eb1fb5df89ad2f225d635a63e8a89
David Griffiths <dgriff_67@yahoo.com>
2016-12-16 16:51:08 +0000

    expanded tag model relations to other models

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $fillable = [ 'name', 'user_id' ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function favourites()
+    {
+        return $this->hasMany(Favourite::class);
+    }
 }
