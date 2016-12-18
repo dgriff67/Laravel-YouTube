99164177201a1ed09d1f04de993a40de62191f78
David Griffiths <dgriff_67@yahoo.com>
2016-12-18 16:05:18 +0000

    home view references local copies of jQuery, Bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,40 +1,12 @@
 <html>
     <head>
         <title>Home Page</title>
+        <link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap.min.css') !!}" >
+        <link rel="stylesheet" href="{!! asset('css/bootstrap-theme.min.css') !!}">
+        <script src="{!! asset('js/bootstrap.min.js') !!}"></script>
+        <script src="//code.jquery.com/jquery-3.1.0.min.js"></script>
 
-        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
-        <style>
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                height: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-                margin-bottom: 40px;
-            }
-            .quote {
-                font-size: 24px;
-            }
-        </style>
     </head>
     <body>
         <div class="container">
