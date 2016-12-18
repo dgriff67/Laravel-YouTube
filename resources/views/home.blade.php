eb5ea8881bcd3a48a57f273d072e50aa98f4f33a
David Griffiths <dgriff_67@yahoo.com>
2016-12-18 17:13:30 +0000

    changed home view to extend newly created master

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,40 +1,11 @@
-<html>
-    <head>
-        <title>Home Page</title>
-        <link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap.min.css') !!}" >
-        <link rel="stylesheet" href="{!! asset('css/bootstrap-theme.min.css') !!}">
-        <script src="{!! asset('js/bootstrap.min.js') !!}"></script>
-        <script src="//code.jquery.com/jquery-3.1.0.min.js"></script>
+@extends('master')
+@section('title', 'Home')
 
-
-    </head>
-    <body>
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">YouTube Favourites</a>
-                </div>
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li><a href="#">YouTube</a></li>
-                        <li><a href="#">Favourites</a></li>
-                        <li><a href"#">Tags</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class="container">
-            <div class="content">
-                <div class="title">Home</div>
-                <div class="quote">Our Home page!</div>
-            </div>
+@section('content')
+    <div class="container">
+        <div class="content">
+            <div class="title">Home Page</div>
+            <div class="quote">Our Home page!</div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,10 +1,11 @@
 <html>
 <head>
     <title> @yield('title') </title>
-    <link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap.min.css') !!}" >
-    <link rel="stylesheet" href="{!! asset('css/bootstrap-theme.min.css') !!}">
-    <script src="{!! asset('js/bootstrap.min.js') !!}"></script>
-    <script src="//code.jquery.com/jquery-3.1.0.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+
+    <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </head>
 <body>
