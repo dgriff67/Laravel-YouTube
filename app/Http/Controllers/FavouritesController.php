bbdaac7a61b0649b0d2af240abc9964e297f2474
David Griffiths <dgriff_67@yahoo.com>
2016-12-18 18:18:55 +0000

    create favourites action moved to favourites controller

--- app/Http/Controllers/FavouritesController.php
+++ app/Http/Controllers/FavouritesController.php
@@ -23,7 +23,7 @@ class FavouritesController extends Controller
      */
     public function create()
     {
-        //
+        return view('favourites.create');
     }
 
     /**
--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -21,9 +21,4 @@ class PagesController extends Controller
         return view('tags');
     }
 
-    public function create()
-    {
-        return view('favourites.create');
-    }
-
 }
