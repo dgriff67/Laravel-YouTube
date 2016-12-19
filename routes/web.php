da16d382a7abecc622d7909c8551738cbcce2c20
David Griffiths <dgriff_67@yahoo.com>
2016-12-19 17:34:21 +0000

    added route for edit favourite

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', 'PagesController@home');
 Route::get('/favourites', 'FavouritesController@index');
 Route::get('/favourite/{id?}', 'FavouritesController@show');
+Route::get('/favourite/{id?}/edit', 'FavouritesController@edit');
 Route::get('/tags', 'PagesController@tags');
 Route::get('/create', 'FavouritesController@create');
 Route::post('/create', 'FavouritesController@store');
