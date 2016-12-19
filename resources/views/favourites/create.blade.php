64b2ee73cb59f52f9c27f1f9caa3493e78829b0a
David Griffiths <dgriff_67@yahoo.com>
2016-12-19 15:17:50 +0000

    added csrf_token and name references for each fieldset element

--- resources/views/favourites/create.blade.php
+++ resources/views/favourites/create.blade.php
@@ -4,26 +4,27 @@
 @section('content')
     <div class="container col-md-8 col-md-offset-2">
         <div class="well well bs-component">
-            <form class="form-horizontal">
+            <form class="form-horizontal" method="post">
+                <input type="hidden" name="_token" value="{!! csrf_token() !!}">
                 <fieldset>
                     <legend>Add a Favourite</legend>
                     <div class="form-group">
                         <label for="title" class="col-lg-2 control-label">Title</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="title" placeholder="Title">
+                            <input type="text" class="form-control" id="title" placeholder="Title" name="title">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="videoid" class="col-lg-2 control-label">Videoid</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="videoid" placeholder="Videoid">
+                            <input type="text" class="form-control" id="videoid" placeholder="Videoid" name="videoid">
                         </div>
                     </div>
 
                     <div class="form-group">
                         <label for="use_id" class="col-lg-2 control-label">user_id</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="user_id" placeholder="User_id">
+                            <input type="text" class="form-control" id="user_id" placeholder="User_id" name="user_id">
                         </div>
                     </div>
 
