53b8fe308148a417ec0a263f95ad18736db298a7
David Griffiths <dgriff_67@yahoo.com>
2016-12-20 17:46:37 +0000

    added tagging facility for creating new tag to show view

--- resources/views/favourites/show.blade.php
+++ resources/views/favourites/show.blade.php
@@ -18,5 +18,41 @@
                 </form>
             </div>
     </div>
+    <div class="well well bs-component">
+                <form class="form-horizontal" method="post" action="/tag">
 
+                    @foreach($errors->all() as $error)
+                        <p class="alert alert-danger">{{ $error }}</p>
+                    @endforeach
+
+                    @if(session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <input type="hidden" name="_token" value="{!! csrf_token() !!}">
+
+                    <fieldset>
+                        <legend>Tag</legend>
+                        <div class="form-group">
+                            <label for="name" class="col-lg-2 control-label">Name</label>
+                            <div class="col-lg-12">
+                                <input type="text" class="form-control" id="name" placeholder="Tag" name="name">
+                            </div>
+                            <label for="name" class="col-lg-2 control-label">User_id</label>
+                            <div class="col-lg-12">
+                                <input type="text" class="form-control" id="user_id" placeholder="User_id" name="user_id">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-lg-10 col-lg-offset-2">
+                                <button type="reset" class="btn btn-default">Cancel</button>
+                                <button type="submit" class="btn btn-primary">Tag</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
 @endsection
