f4e6c25f44408b2dd4da05b36cbb21ebd52ef434
David Griffiths <dgriff_67@yahoo.com>
2016-12-20 16:54:32 +0000

    changed from fillable to guarded in Tag model

--- app/Tag.php
+++ app/Tag.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    protected $fillable = [ 'name', 'user_id' ];
+    protected $guarded = ['id'];
 
     public function user()
     {
