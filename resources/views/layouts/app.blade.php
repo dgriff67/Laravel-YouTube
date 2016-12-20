479735e6caddfd8494aaf3b4ad9ded855e725c58
David Griffiths <dgriff_67@yahoo.com>
2016-12-20 18:55:12 +0000

    changed yield directive from pageTitle to title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('pageTitle') - YouTube Favourites</title>
+    <title>@yield('title')</title>
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
