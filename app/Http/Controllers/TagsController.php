96de25dc472cf10da709e7be9202b939c23e93f3
David Griffiths <dgriff_67@yahoo.com>
2016-12-20 17:04:57 +0000

    inserted and implemented newTag action of tags controller

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -4,7 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests;
+
+use App\Http\Requests\TagFormRequest;
+
+use App\Tag;
+
 class TagsController extends Controller
 {
-    //
+    public function newTag(TagFormRequest $request)
+    {
+        $tag = new Tag(array(
+            'name' => $request->get('name'),
+            'user_id' => $request->get('user_id'),
+        ));
+
+        $tag->save();
+
+        return redirect()->back()->with('status', 'Your tag has been created!');
+    }
 }
