5644ba13747e1aa5aa6b58a3f3d42b4125082552
David Griffiths <dgriff_67@yahoo.com>
2016-12-20 16:05:51 +0000

    added route for deleting favourite

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ Route::get('/favourites', 'FavouritesController@index');
 Route::get('/favourite/{id?}', 'FavouritesController@show');
 Route::get('/favourite/{id?}/edit', 'FavouritesController@edit');
 Route::post('/favourite/{id?}/edit', 'FavouritesController@update');
+Route::get('/favourite/{id?}/delete', 'FavouritesController@destroy');
+
 Route::get('/tags', 'PagesController@tags');
 Route::get('/create', 'FavouritesController@create');
 Route::post('/create', 'FavouritesController@store');
