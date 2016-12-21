6a275c02449b209bb74d69806aae2053c3862111
David Griffiths <dgriff_67@yahoo.com>
2016-12-21 15:39:03 +0000

    added route for tags and spacing to show how routes are paired more clearly, including get-post pairs for the same url

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,20 @@
 Route::get('/', 'PagesController@home');
 Route::get('/home', 'HomeController@home');
 Route::get('/favourites', 'FavouritesController@index');
+
 Route::get('/favourite/{id?}', 'FavouritesController@show');
+
 Route::get('/favourite/{id?}/edit', 'FavouritesController@edit');
 Route::post('/favourite/{id?}/edit', 'FavouritesController@update');
+
 Route::post('/favourite/{id?}/delete', 'FavouritesController@destroy');
+
 Route::get('users/register', 'Auth\RegisterController@showRegistrationForm');
 Route::post('users/register', 'Auth\RegisterController@register');
+
 Route::post('/tag', 'TagsController@newTag');
 Route::get('/tags', 'TagsController@index');
+
 Route::get('/create', 'FavouritesController@create');
 Route::post('/create', 'FavouritesController@store');
 
