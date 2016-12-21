0d79e9a4e64f128349cf7a4985dd7e98ea8aebb7
David Griffiths <dgriff_67@yahoo.com>
2016-12-21 16:08:46 +0000

    changed notation of belongs to call

--- app/Favourite.php
+++ app/Favourite.php
@@ -10,7 +10,7 @@ class Favourite extends Model
 
     public function user()
     {
-        return $this->belongsTo('App\User');
+        return $this->belongsTo(User::class);
     }
 
     public function tags()
