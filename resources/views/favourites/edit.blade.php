aef5141ddebc1de7a0fd34548a92a4c462126a08
David Griffiths <dgriff_67@yahoo.com>
2016-12-21 16:34:27 +0000

    removed videoid from edit favourite - functionality will be to change title and to add tags not change videoid

--- resources/views/favourites/edit.blade.php
+++ resources/views/favourites/edit.blade.php
@@ -27,13 +27,7 @@
                             <input type="text" class="form-control" id="title" name="title" value="{!! $favourite->title !!}">
                         </div>
                     </div>
-                    <div class="form-group">
-                        <label for="content" class="col-lg-2 control-label">videoid</label>
-                        <div class="col-lg-10">
-                            <input type="text" class="form-control" id="title" name="videoid" value="{!! $favourite->videoid !!}">
-                        </div>
-                    </div>
-
+                    
                     <div class="form-group">
                         <div class="col-lg-10 col-lg-offset-2">
                             <button class="btn btn-default">Cancel</button>
