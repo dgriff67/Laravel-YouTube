1a4ed663e3d1e07b9da485cc3bae95f443a359c2
David Griffiths <dgriff_67@yahoo.com>
2016-12-21 16:14:12 +0000

    added hasMany relationships to user model between user and favourites and tags

--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function favourites() {
+      return $this->hasMany('App\Favourite');
+    }
+
+    public function tags() {
+      return $this->hasMany('App\Tag');
+    }
 }
