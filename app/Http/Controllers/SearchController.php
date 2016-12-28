b1675177f0d51e3cd8c585d1daa1b16ffb72ca7d
David Griffiths <dgriff_67@yahoo.com>
2016-12-28 17:37:03 +0000

    implemented results action of search controler using Google sample API code

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Auth;
+use Google_Client;
+use Google_Service_YouTube;
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
 class SearchController extends Controller
 {
@@ -22,7 +26,25 @@ class SearchController extends Controller
 
     public function results(Request $request)
     {
+        $DEVELOPER_KEY = 'AIzaSyCiFuJxg2zt20dZgRyw5Q7w1sajIORrBEg';
 
-        return view('favourites.search', ['searchResults'=> $searchResults]);
+        $client = new Google_Client();
+        $client->setDeveloperKey($DEVELOPER_KEY);
+        // Define an object that will be used to make all API requests.
+        $youtube = new Google_Service_YouTube($client);
+        try {
+            $searchResponse = $youtube->search->listSearch('id,snippet',
+            array(
+              'q' => $request->get('q'),
+              'maxResults' => $request->get('maxResults')
+            ));
+        } catch (Google_Service_Exception $e) {
+            $e->getMessage();
+        } catch (Google_Exception $e) {
+            $e->getMessage();
+        }
+
+
+        return view('favourites.search', ['searchResponse'=> $searchResponse]);
     }
 }
