77fd95a6507b9a7bc01b159bfa0324afe44a54e7
David Griffiths <dgriff_67@yahoo.com>
2016-12-30 17:57:47 +0000

    added search link in navbar

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -12,6 +12,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
+                <li><a href="{{ url('/search') }}">Search</a></li>
                 <li><a href="{{ url('/favourites') }}">Favourites</a></li>
                 <li><a href="{{ url('/tags') }}">Tags</a></li>
             </ul>
