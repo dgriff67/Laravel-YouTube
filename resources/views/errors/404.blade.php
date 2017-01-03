6f44cf9816bfc3669811d484888c9f8406ec0e7d
David Griffiths <dgriff_67@yahoo.com>
2017-01-03 16:28:41 +0000

    custom view for 404 not found page created

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('title', 'Not Found Error')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Error Detail</div>
+
+                <div class="panel-body">
+                    This page was not found!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
