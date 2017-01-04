dcec4d1a19b0841e6d8c9d1c28356b0bad9b3b4a
David Griffiths <dgriff_67@yahoo.com>
2017-01-04 16:14:11 +0000

    added session status and error messages to show favourite view

--- resources/views/favourites/show.blade.php
+++ resources/views/favourites/show.blade.php
@@ -1,17 +1,16 @@
 @extends('layouts.app')
 @section('title', 'View a favourite')
 @section('content')
-<!--<div style="overflow:hidden;height:270px;width:480px;max-width:100%;max-height:100%;">
-    <div class="container col-md-4">
-        <div id="youtube_canvas" style="height:100%;width:100%;max-width:100%">
-        <iframe style="height:100%;width:100%;max-width:100%;border:0;" frameborder="0" src="https://www.youtube.com/embed/{!! $favourite->videoid !!}">
-        </iframe>
-    </div>
-    <a class="youtube-embed-code" href="http://dedicatedhosting.pro" id="get-youtube-data">dedicated hosting reviews</a>
-    <style>#youtube_canvas img{max-width:none!important;background:none!important}</style>
-</div>-->
     <div class="container col-md-4">
             <div class="well well bs-component">
+                @foreach ($errors->all() as $error)
+                    <p class="alert alert-danger">{{ $error }}</p>
+                @endforeach
+                @if (session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                @endif
                 <div>
                     @if ($favourite->kind == 'youtube#video')
                         <iframe frameborder="0" src="https://www.youtube.com/embed/{!! $favourite->videoid!!}?autoplay=1" allowfullscreen="allowfullscreen" width="320" height="240" frameborder="0">
