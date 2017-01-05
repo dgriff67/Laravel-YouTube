02ef264aedcb08ae47e38c65490236089856dab8
David Griffiths <dgriff_67@yahoo.com>
2017-01-05 16:31:57 +0000

    added search play action form to search index view using blade Form fascade. Also tidied up favourite create form of same view using blade Form fascade

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -35,48 +35,54 @@
             @if ($searchResponse === NULL)
             @else
                 <table class="table">
-                    <thead>
+                    @foreach($searchResponse as $searchResult)
                         <tr>
-                            <th>Image</th>
-                            <th>Title</th>
-                            <th>Action</th>
-
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($searchResponse as $searchResult)
-                            <tr>
-                                <td>
-                                    <img src="{!! $searchResult['snippet']['thumbnails']['default']['url'] !!}" alt="{!! $searchResult['snippet']['title'] !!}" style="width:width;height:height;">
-                                </td>
-                                <td>
-                                    {!! $searchResult['snippet']['title'] !!}
-                                </td>
-                                <td>
-                                    <form action="{{ route('favourite.create') }}" method="post">
-                                        <input type="hidden" name="_token" value="{!! csrf_token() !!}">
+                            <td>
+                                <form action="{{ route('search.play') }}" method="get">
+                                {{ csrf_field() }}
                                         <div class="form-group">
-                                            <input type="hidden" value="{!! $searchResult['snippet']['title'] !!}" name="title">
-                                            <input type="hidden" value="{!! $searchResult['snippet']['thumbnails']['default']['url'] !!}" name="imageUrl">
-                                            <input type="hidden" value="{!! $searchResult['id']['kind'] !!}" name="kind">
+                                            {{ Form::hidden('title', $searchResult['snippet']['title']) }}
+                                            {{ Form::hidden('imageUrl', $searchResult['snippet']['thumbnails']['default']['url']) }}
+                                            {{ Form::hidden('kind', $searchResult['id']['kind']) }}
                                             @if ($searchResult['id']['kind'] =='youtube#video')
-                                                <input type="hidden" value="{!! $searchResult['id']['videoId'] !!}" name="videoid">
+                                                {{ Form::hidden('videoid', $searchResult['id']['videoId']) }}
                                             @endif
                                             @if ($searchResult['id']['kind'] =='youtube#channel')
-                                                <input type="hidden" value="{!! $searchResult['id']['channelId'] !!}" name="videoid">
+                                                {{ Form::hidden('videoid', $searchResult['id']['channelId']) }}
                                             @endif
                                             @if ($searchResult['id']['kind'] =='youtube#playlist')
-                                                <input type="hidden" value="{!! $searchResult['id']['playlistId'] !!}" name="videoid">
+                                                {{ Form::hidden('videoid', $searchResult['id']['playlistId']) }}
                                             @endif
                                         </div>
-                                        <button type="submit" class="btn btn-primary">Add Favourite</button>
+                                    <input type="image" src="{!! $searchResult['snippet']['thumbnails']['default']['url'] !!}" alt="{!! $searchResult['snippet']['title'] !!}" style="width:width;height:height;">
+                                </form>
+                            </td>
+                            <td>
+                                {!! $searchResult['snippet']['title'] !!}
+                            </td>
+                            <td>
+                                <form action="{{ route('favourite.create') }}" method="post">
+                                    {{ csrf_field() }}
+                                        <div class="form-group">
+                                            {{ Form::hidden('title', $searchResult['snippet']['title']) }}
+                                            {{ Form::hidden('imageUrl', $searchResult['snippet']['thumbnails']['default']['url']) }}
+                                            {{ Form::hidden('kind', $searchResult['id']['kind']) }}
+                                            @if ($searchResult['id']['kind'] =='youtube#video')
+                                                {{ Form::hidden('videoid', $searchResult['id']['videoId']) }}
+                                            @endif
+                                            @if ($searchResult['id']['kind'] =='youtube#channel')
+                                                {{ Form::hidden('videoid', $searchResult['id']['channelId']) }}
+                                            @endif
+                                            @if ($searchResult['id']['kind'] =='youtube#playlist')
+                                                {{ Form::hidden('videoid', $searchResult['id']['playlistId']) }}
+                                            @endif
+                                        </div>
+                                    <button type="submit" class="btn btn-primary">Add Favourite</button>
 
-                                    </form>
-                                </td>
-                            </tr>
-
-                        @endforeach
-                    </tbody>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
                 </table>
             @endif
         </div>
