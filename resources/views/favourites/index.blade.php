881ed5b3c6cece552e711e2e0df2a726b5d44270
David Griffiths <dgriff_67@yahoo.com>
2017-01-05 17:36:10 +0000

    adjustments made to layout of favourites index view

--- resources/views/favourites/index.blade.php
+++ resources/views/favourites/index.blade.php
@@ -2,9 +2,9 @@
 @section('title', 'View all Favourites')
 @section('content')
 
-    <div class="container col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">
+    <div class="container col-md-8">
+            <div class="well well bs-component">
+                <div>
                     <h2> Favourites </h2>
                 </div>
                 @if (session('status'))
@@ -16,34 +16,24 @@
                     <p> There are no favourites.</p>
                 @else
                     <table class="table">
-                        <thead>
+                        @foreach($favourites as $favourite)
                             <tr>
-                                <th>ID</th>
-                                <th>Title</th>
-                                <th>Videoid</th>
+                                <td>
+                                    @if ($favourite->kind == 'youtube#channel')
+                                        <a href="https://www.youtube.com/channel/{!! $favourite->videoid !!}" target='_blank'><img src="{!! $favourite->imageUrl !!}" alt="{!! $favourite->title !!}" style="width:width;height:height;"></a>
+                                    @else
+                                        <a href="{!! action('FavouritesController@show', $favourite->id) !!}"><img src="{!! $favourite->imageUrl !!}" alt="{!! $favourite->title !!}" style="width:width;height:height;"></a>
+                                    @endif
+                                </td>
+                                <td>
+                                    @if ($favourite->kind == 'youtube#channel')
+                                        <a href="https://www.youtube.com/channel/{!! $favourite->videoid !!}" target='_blank'>{!! $favourite->title !!} <br> {!!$favourite->tag_string !!}</a>
+                                    @else
+                                        <a href="{!! action('FavouritesController@show', $favourite->id) !!}">{!! $favourite->title !!} <br>{!!$favourite->tag_string !!} </a>
+                                    @endif
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($favourites as $favourite)
-                                <tr>
-                                    <td>
-                                        @if ($favourite->kind == 'youtube#channel')
-                                            <a href="https://www.youtube.com/channel/{!! $favourite->videoid !!}" target='_blank'><img src="{!! $favourite->imageUrl !!}" alt="{!! $favourite->title !!}" style="width:width;height:height;"></a>
-                                        @else
-                                            <a href="{!! action('FavouritesController@show', $favourite->id) !!}"><img src="{!! $favourite->imageUrl !!}" alt="{!! $favourite->title !!}" style="width:width;height:height;"></a>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if ($favourite->kind == 'youtube#channel')
-                                            <a href="https://www.youtube.com/channel/{!! $favourite->videoid !!}" target='_blank'>{!! $favourite->title !!} <br> {!!$favourite->tag_string !!}</a>
-                                        @else
-                                            <a href="{!! action('FavouritesController@show', $favourite->id) !!}">{!! $favourite->title !!} <br>{!!$favourite->tag_string !!} </a>
-                                        @endif
-                                    </td>
-                                    <td>{!! $favourite->videoid !!}</td>
-                                </tr>
-                            @endforeach
-                        </tbody>
+                        @endforeach
                     </table>
                 @endif
             </div>
