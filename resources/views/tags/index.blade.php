a61e87314266bebc5c63583d628b6c7eb8610129
David Griffiths <dgriff_67@yahoo.com>
2017-01-06 13:20:43 +0000

    tidied up layout of tags index view

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -2,26 +2,27 @@
 @section('title', 'View all Tags')
 @section('content')
 
-    <div class="container col-md-8 col-md-offset-2">
-        <form class="form-horizontal" method="post">
-            <input type="hidden" name="_token" value="{!! csrf_token() !!}">
-            <fieldset>
-                <legend>Add a Tag</legend>
-                <div class="form-group">
-                    <label for="name" class="col-lg-2 control-label">Name</label>
-                    <div class="col-lg-10">
-                        <input type="text" class="form-control" id="name" placeholder="Name" name="name">
+    <div class="container col-md-8">
+        <div class="well well bs-component">
+            <form class="form-horizontal" method="post">
+                <input type="hidden" name="_token" value="{!! csrf_token() !!}">
+                <fieldset>
+                    <legend>Add a Tag</legend>
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <div>
+                            <input type="text" class="form-control" id="name" placeholder="Name" name="name">
+                        </div>
                     </div>
-                </div>
 
-                <div class="form-group">
-                    <div class="col-lg-10 col-lg-offset-2">
-                        <button class="btn btn-default">Cancel</button>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                    <div class="form-group">
+                        <div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <button class="btn btn-default">Cancel</button>
+                        </div>
                     </div>
-                </div>
-            </fieldset>
-        </form>
+                </fieldset>
+            </form>
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h2> Tags </h2>
@@ -35,27 +36,17 @@
                     <p> There are no tags.</p>
                 @else
                     <table class="table">
-                        <thead>
+                        @foreach($tags as $tag)
                             <tr>
-                                <th>ID</th>
-                                <th>Title</th>
+                                <td>
+                                    {!! $tag->name !!}
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($tags as $tag)
-                                <tr>
-                                    <td>{!! $tag->id !!} </td>
-                                    <td>
-                                        {!! $tag->name !!}
-                                    </td>
-
-                                </tr>
-                            @endforeach
-                        </tbody>
+                        @endforeach
                     </table>
                 @endif
             </div>
-
+        </div>
     </div>
 
 @endsection
