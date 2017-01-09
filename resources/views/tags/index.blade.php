2d83dca2d23155f874d9e3d5d0e8505e820de1f5
David Griffiths <dgriff_67@yahoo.com>
2017-01-09 18:07:41 +0000

    removed new tag form from tags index view - new tags created now when updating favourites

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -4,25 +4,6 @@
 
     <div class="container col-md-8">
         <div class="well well bs-component">
-            <form class="form-horizontal" method="post">
-                <input type="hidden" name="_token" value="{!! csrf_token() !!}">
-                <fieldset>
-                    <legend>Add a Tag</legend>
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <div>
-                            <input type="text" class="form-control" id="name" placeholder="Name" name="name">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                            <button class="btn btn-default">Cancel</button>
-                        </div>
-                    </div>
-                </fieldset>
-            </form>
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h2> Tags </h2>
