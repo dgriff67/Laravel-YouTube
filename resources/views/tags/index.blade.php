2a6686fdd9a2e1adf6934a63ef6fd1401d4410d7
David Griffiths <dgriff_67@yahoo.com>
2017-01-09 18:48:20 +0000

    the tags controller now passes a randon favourite associated to each tag. The view now displays the imageUrl associated with this favourite alongside the tag

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -14,11 +14,16 @@
                     </div>
                 @endif
                 @if ($tags->isEmpty())
-                    <p> There are no tags.</p>
+                    <p> There are no tags. Create a tag by editing a favourite.</p>
                 @else
                     <table class="table">
                         @foreach($tags->sortBy('name') as $tag)
                             <tr>
+                                <td>
+                                    @foreach($tag->favourite as $favourite)
+                                        <a href="{!! action('TagsController@show', $tag->id) !!}"><img src="{!! $favourite->imageUrl !!}" alt="{{ $favourite->title }}" style="width:width;height:height;"></a>
+                                    @endforeach
+                                </td>
                                 <td>
                                     <a href="{!! action('TagsController@show', $tag->id) !!}">{{ $tag->name }} </a>
                                 </td>
